a7de2ecc02ac8cf02297eea51b313b657bfbb5f0
shuup777 <pashuuphehey@gmail.com>
2025-12-12 06:36:30 +0700

    Update settings.py

--- mify/settings.py
+++ mify/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'user.User'
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,4 +124,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # Tell Django where to find project-level static files during development
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
