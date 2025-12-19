543f67443707165485252693b99048dff4289dd9
shuup777 <pashuuphehey@gmail.com>
2025-12-19 07:47:36 +0700

    Set DEBUG to False and added CSRF trusted origins
    Changed DEBUG setting to False for production.

--- mify/settings.py
+++ mify/settings.py
@@ -24,9 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=6od5dns1rlrnz85tiv@&vp$2uqeryw3efho@b!=j(#+lb5-f('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://mifyy.up.railway.app",
+    "https://mify.com",
+]
 
 
 # Application definition
